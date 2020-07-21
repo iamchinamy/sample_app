dd18f7a07157c1e99fa034895f78bf1ef68b1d88
vagrant <vagrant@localhost.localdomain>
2020-07-21 09:03:10 +0000

    3-11 htmlを編集

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e62f8383c274954afb65dadd198ddaf09e3a41549d7ffe717dffa5e8675f44a0abca0f53b74bad42c1b571ad1e15c063b79ed6299c1bb587194dd0d1c54ff77a'
+
